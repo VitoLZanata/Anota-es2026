64d4113139c1a4fdf1dab6e86cfcbb2d44156cf2
VitoLZanata <vitinhozanata123@gmail.com>
2026-03-17 09:33:00 -0300

    aaaaaa

--- client.py
+++ client.py
@@ -1,10 +1,18 @@
-# Cliente
+# Servidor
 import socket
 
-HOST = "192.168.246.106"
+HOST = "0.0.0.0"
 PORT = 9002
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"ola mundo")
-    print(s.recv(1024).decode())
+    s.bind((HOST, PORT))
+    while(True):
+        s.listen(1)
+        conn, addr = s.accept()
+        print("Cliente: " , addr)
+        with conn:
+            while(True):
+                data = conn.recv(1024)
+                result = eval(data)
+                msg = (str)(result)
+                conn.sendall(msg.encode('utf-8'))
